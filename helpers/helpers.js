f04454438446d1b2732d62e1c9300e61cdaae4ad
Jon Gold <hello@designedbygold.com>
2014-05-18 13:20:47 +0200

    niceDesc

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,3 +2,22 @@ UI.registerHelper('wrapperClass', function(currentUserActive) {
   return currentUserActive ? "wrapper wrapper--active" : "wrapper wrapper--inactive";
 });
 
+UI.registerHelper('niceDesc', function(role) {
+  switch (role) {
+    case "beta":
+      return "bass BP filter"
+      break;
+    case "gamma":
+      return "bass LFO"
+      break;
+    case "xShift":
+      return "master pan"
+      break;
+    case "zShift":
+      return "breakbeat LP filter"
+      break;
+    default:
+      return "×"
+      break;
+  }
+});
